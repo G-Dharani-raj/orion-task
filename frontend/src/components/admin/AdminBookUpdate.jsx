4a80ae865d10ea14b1aaebfc09b74d457172e1d0
G-Dharani-raj <gdharaniraj0@gmail.com>
2023-06-27 13:31:19 +0530

    changed the api to the deployed

--- frontend/src/components/admin/AdminBookUpdate.jsx
+++ frontend/src/components/admin/AdminBookUpdate.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AdminBookUpdate = () => {
-	return <div>AdminBookUpdate</div>;
+	const { name, author, cover, _id } = JSON.parse(
+		localStorage.getItem("book")
+	);
+	const [new_name, setName] = useState(name);
+	const [new_author, setAuthor] = useState(author);
+	const [new_cover, setCover] = useState(cover);
+
+	return (
+		<div>
+			<form>
+				<label htmlFor="name">Name</label>
+				<input
+					type="text"
+					name="name"
+					value={new_name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+			</form>
+		</div>
+	);
 };
 
 export default AdminBookUpdate;
